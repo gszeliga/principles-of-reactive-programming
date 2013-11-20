24b1a2090e6f17fefbf6970fd455fa51ac973502
Guillermo Szeliga <gszeliga@gmail.com>
2013-11-20 20:03:54 +0800

    - Still working on the simulation assignment. Doesn't work as expected.

--- assignments/simulations/.worksheet/src/simulations.test.scala
+++ assignments/simulations/.worksheet/src/simulations.test.scala
@@ -1,6 +1,6 @@
 package simulations
 
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(702); 
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(700); 
 
   def getControlWireSignals(source: Int, cws: Int) = {
 
@@ -19,13 +19,31 @@ object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; 
         control(current / 2, reminder :: binary)
       }
     }
-		
+
     control(source, Nil).reverse.view.zipWithIndex.force
   };System.out.println("""getControlWireSignals: (source: Int, cws: Int)Seq[(Int, Int)]""");$skip(31); val res$0 = 
 
   getControlWireSignals(0, 2);System.out.println("""res0: Seq[(Int, Int)] = """ + $show(res$0));$skip(30); val res$1 = 
   getControlWireSignals(1, 2);System.out.println("""res1: Seq[(Int, Int)] = """ + $show(res$1));$skip(30); val res$2 = 
   getControlWireSignals(2, 2);System.out.println("""res2: Seq[(Int, Int)] = """ + $show(res$2));$skip(30); val res$3 = 
-  getControlWireSignals(3, 2);System.out.println("""res3: Seq[(Int, Int)] = """ + $show(res$3))}
+  getControlWireSignals(3, 2)
+
+  import math.random;System.out.println("""res3: Seq[(Int, Int)] = """ + $show(res$3));$skip(274); 
+
+  def probabilityExpression(probability: Int) = {
+
+    var pending = probability
+
+    (1 to 100) map { i =>
+      val flag = (2 * random).toInt
+
+      if (flag == 1 && pending > 0) {
+        pending = pending - 1
+        flag
+      } else 0
+    }
+  };System.out.println("""probabilityExpression: (probability: Int)scala.collection.immutable.IndexedSeq[Int]""");$skip(29); val res$4 = 
+
+  probabilityExpression(25);System.out.println("""res4: scala.collection.immutable.IndexedSeq[Int] = """ + $show(res$4))}
 
 }
--- assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -17,7 +17,24 @@ class EpidemySimulator extends Simulator {
 
   import SimConfig._
 
-  val persons: List[Person] = createPupulation(SimConfig.population) // to complete: construct list of persons
+  val persons: List[Person] = createPupulation(300) // to complete: construct list of persons
+
+  def probabilityExpression(probability: Int) = {
+
+    var pending = probability
+
+    (1 to 100) map { i =>
+      val flag = (2 * random).toInt
+
+      if (flag == 1 && pending > 0) {
+        pending = pending - 1
+        flag
+      } else 0
+    }
+  }
+
+  val probabilityOfInfection = probabilityExpression(40)
+  val probabilityOfDeath = probabilityExpression(25)
 
   class Person(val id: Int) {
     var infected = false
@@ -29,39 +46,127 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    def changePosition(delay: Int): Unit = {
+    var infectedAt: Int = 0
+
+    def nextMove() = ((5 * random).toInt) + 1
+
+    def relocate(delay: Int): Unit = {
 
       afterDelay(delay)({
 
+        val currentPersonsDistribution: List[Person] = persons
+
+        val currentDay = delay
         val currenRow = row
         val currenCol = col
 
-        val aboveRow = if ((currenRow - 1) <= 0) SimConfig.roomRows else currenRow - 1
-        val belowRow = if ((currenRow + 1) >= SimConfig.roomRows) 1 else currenRow + 1
+        val isCurrentlyInfected = infected
+        val elapsedTimeSinceInfection = if (isCurrentlyInfected) (currentDay - infectedAt) else 0
+        val isCurrentlySick = sick
+        val isCurrentlyDead = dead
+        val isCurrentlyImmune = elapsedTimeSinceInfection >= 16 && elapsedTimeSinceInfection <= 18 && !isCurrentlySick
+        val mustBeHealthy = isCurrentlyImmune && elapsedTimeSinceInfection >= 18
 
-        val aboveCol = if ((currenCol - 1) <= 0) SimConfig.roomColumns else currenCol - 1
-        val belowCol = if ((currenCol + 1) >= SimConfig.roomColumns) 1 else currenCol + 1
+        if (mustBeHealthy) {
 
-        row = if (Random.nextInt >= 0) aboveRow else belowRow
-        col = if (Random.nextInt >= 0) aboveCol else belowCol
+          infected = false
+          sick = false
+          immune = false
+          infectedAt = 0
 
-        changePosition(delay + (5 * random).toInt)
-        
+          println(s"Person ${id} is health again!")
+
+        } else {
+
+          immune = isCurrentlyImmune
+
+          if (isCurrentlyInfected && !isCurrentlySick && !isCurrentlyImmune) {
+
+            sick = elapsedTimeSinceInfection >= 6
+
+            if (sick) {
+              println(s"Person ${id} got sick after ${elapsedTimeSinceInfection} days!!")
+            }
+          }
+
+          if (isCurrentlySick && !isCurrentlyDead) {
+
+            dead = ((delay - infectedAt) > 14) && (probabilityOfDeath((100 * random).toInt) == 1)
+
+            if (dead) {
+              println(s"Person ${id} died after ${elapsedTimeSinceInfection} days")
+            }
+          }
+        }
+
+        if (!dead) {
+
+          def nextCoordinate(current: Int, edge: Int)(reachedTheEdge: Int => Boolean)(next: Int => Int) = {
+            if (reachedTheEdge(current)) edge else next(current)
+          }
+
+          val aboveRow = nextCoordinate(currenRow, SimConfig.roomRows)(_ == 0) { _ - 1 }
+          val belowRow = nextCoordinate(currenRow, 0)(_ == SimConfig.roomRows) { _ + 1 }
+
+          val aboveCol = nextCoordinate(currenCol, SimConfig.roomColumns)(_ == 0) { _ - 1 }
+          val belowCol = nextCoordinate(currenCol, 0)(_ == SimConfig.roomColumns) { _ + 1 }
+
+          val availableRooms = List((aboveCol, currenRow), (belowCol, currenRow), (currenCol, aboveRow), (currenCol, belowRow))
+
+          if (!isCurrentlyInfected) {
+
+            val notVisibleInfectedRooms = availableRooms.filter(pos => currentPersonsDistribution.filter(p => p.row == pos._2 && p.col == pos._1 && (p.sick || p.dead) && p.id != id).isEmpty)
+
+            if (!notVisibleInfectedRooms.isEmpty) {
+
+              val cleanRoom = notVisibleInfectedRooms((notVisibleInfectedRooms.size * random).toInt)
+
+              row = cleanRoom._2
+              col = cleanRoom._1
+
+              val theresInfectedPeople = !currentPersonsDistribution.filter(p => p.row == row && p.col == col && p.infected && p.id != id).isEmpty
+
+              if (theresInfectedPeople) {
+                infected = probabilityOfInfection((100 * random).toInt) == 1
+
+                if (infected) {
+                  infectedAt = currentDay
+                  println(s"Person ${id} got infected at day ${infectedAt}!!")
+                }
+              }
+
+              relocate(currentDay + nextMove)
+            }
+          } else if (isCurrentlyInfected) {
+            val anyRoom = availableRooms((availableRooms.size * random).toInt)
+
+            row = anyRoom._2
+            col = anyRoom._1
+
+            relocate(currentDay + nextMove)
+          }
+        }
       })
     }
-    
-    changePosition((5 * random).toInt)
-    //
-    // to complete with simulation logic
-    //
+
+    relocate(nextMove)
+
   }
 
   def createPupulation(size: Int): List[Person] = {
 
+    val luckyOne = (size * random).toInt + 1
+
+    println(s"The initial infected one is '${luckyOne}'")
+
     def populate(tmp: List[Person], pending: Int): List[Person] =
       {
         if (pending == 0) tmp
-        else populate(new Person(pending) :: tmp, pending - 1)
+        else {
+          val p = new Person(pending)
+          p.infected = pending == luckyOne
+          populate(p :: tmp, pending - 1)
+        }
       }
 
     populate(Nil, size)
