c1ec1ab186f48565ed42ceecf604d6d5c140042b
Guillermo Szeliga <gszeliga@gmail.com>
2013-11-21 21:56:30 +0800

    - Some improvements were added according to the specification.

--- assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -52,18 +52,18 @@ class EpidemySimulator extends Simulator {
   }
 
   trait State {
-    def evaluate(currentDay: Int, person: Person)
+    def apply(currentDay: Int, person: Person)
   }
 
   object Initial extends State {
-    def evaluate(currentDay: Int, person: Person) = {
+    def apply(currentDay: Int, person: Person) = {
       val status = if (person.infected) Infected else Healthy
-      status.evaluate(currentDay, person)
+      status.apply(currentDay, person)
     }
   }
 
   object Healthy extends State {
-    def evaluate(currentDay: Int, person: Person) = {
+    def apply(currentDay: Int, person: Person) = {
 
       val notVisibleInfectedRooms = availableRooms(person.row, person.col).filter(room => persons.filter(p => p.row == room._2 && p.col == room._1 && (p.sick || p.dead)).isEmpty)
 
@@ -94,64 +94,57 @@ class EpidemySimulator extends Simulator {
   }
 
   object Infected extends State {
-    def evaluate(currentDay: Int, person: Person) = {
+    def apply(currentDay: Int, person: Person) = {
       person.infectedFor = person.infectedFor + currentDay
 
       println(s"Person ${person.id} has been infected for ${person.infectedFor}")
+
+      person.immune = (person.infectedFor > 16 && person.infectedFor <= 18)
+      person.sick = !person.immune && person.infectedFor > 6
+      person.dead = !person.immune && (person.infectedFor > 14) && (probabilityOfDeath((100 * random).toInt) == 1)
+
+      if (person.dead) {
+        println(s"Person ${person.id} died")
+        person.sick = false
+        person.infected = false
+        person.immune = false
+        person.status = Dead
+      } else {
+        val anyRoom = availableRooms(person.row, person.row)((4 * random).toInt)
+
+        person.row = anyRoom._2
+        person.col = anyRoom._1
+
+        if (person.immune) {
+          println(s"Person ${person.id} is now immune")
+          person.status = Immune
+        } else person.status = Infected
+      }
+    }
+  }
+
+  object Immune extends State {
+    def apply(currentDay: Int, person: Person) = {
+
+      person.infectedFor = person.infectedFor + currentDay
 
       val anyRoom = availableRooms(person.row, person.row)((4 * random).toInt)
 
       person.row = anyRoom._2
       person.col = anyRoom._1
 
-      person.sick = person.infectedFor >= 6
-
-      if (person.sick) {
-        println(s"Person ${person.id} got sick")
-        person.status = Sick
-      } else if (person.infectedFor >= 16 && person.infectedFor <= 18) {
-        println(s"Person ${person.id} is now immune")
-        person.immune = true
-        person.status = Immune
-      } else person.status = Infected
-    }
-  }
-
-  object Immune extends State {
-    def evaluate(currentDay: Int, person: Person) = {
-
-      if (person.infectedFor >= 18) {
+      if (person.infectedFor > 18) {
         person.immune = false
         person.infected = false
+        person.sick = false
         person.infectedFor = 0
         person.status = Healthy
       }
     }
   }
 
-  object Sick extends State {
-    def evaluate(currentDay: Int, person: Person) = {
-
-      person.infectedFor = person.infectedFor + currentDay
-      println(s"Person ${person.id} has been sick for ${person.infectedFor}")
-
-      val anyRoom = availableRooms(person.row, person.col)((4 * random).toInt)
-
-      person.row = anyRoom._2
-      person.col = anyRoom._1
-
-      person.dead = (person.infectedFor > 14) && (probabilityOfDeath((100 * random).toInt) == 1)
-
-      if (person.dead) {
-        println(s"Person ${person.id} died")
-        person.status = Dead
-      } else person.status = Sick
-    }
-  }
-
   object Dead extends State {
-    def evaluate(currentDay: Int, person: Person) = {
-
+    def apply(currentDay: Int, person: Person) = {
     }
   }
 
@@ -170,31 +163,28 @@ class EpidemySimulator extends Simulator {
 
     def nextMove() = ((5 * random).toInt) + 1
 
-    def relocate(days: Int, person: Person): Unit = {
+    def relocate(days: Int): Unit = {
 
       afterDelay(days)({
-        val currentPerson = person
-        currentPerson.status.evaluate(days, currentPerson)
-        relocate(nextMove, person)
+        status.apply(days, this)
+        relocate(nextMove)
       })
     }
 
-    relocate(nextMove, this)
+    relocate(nextMove)
 
   }
 
   def createPupulation(size: Int): List[Person] = {
 
-    val luckyOne = (size * random).toInt + 1
-
-    println(s"The initial infected one is '${luckyOne}'")
+    val prevalenceRate = probabilityExpression(1)
 
     def populate(tmp: List[Person], pending: Int): List[Person] =
       {
         if (pending == 0) tmp
         else {
           val p = new Person(pending)
-          p.infected = pending == luckyOne
+          p.infected = prevalenceRate((prevalenceRate.size * random).toInt) == 1
           populate(p :: tmp, pending - 1)
         }
       }
